876ac93f2b4fbc8ac78ed94d07ca3a12386485ea
ithunter101 <104503223+ithunter101@users.noreply.github.com>
2022-08-30 10:23:58 +0200

    show modal and update setting

--- app/Http/Controllers/Admin/Announcement/SettingController.php
+++ app/Http/Controllers/Admin/Announcement/SettingController.php
@@ -17,4 +17,27 @@ class SettingController extends Controller
             'settings' => $settings,
         ]);
     }
+
+    public function update(Request $request, AnnouncementSettings $setting)
+    {
+        try {
+            $setting->use_password = $request->use_password;
+            $setting->use_title = $request->use_title;
+            $setting->use_state = $request->use_state;
+            $setting->write_permission = $request->write_permission;
+            $setting->view_all = $request->view_all;
+            $setting->comment_permission = $request->comment_permission;
+            $setting->is_using = $request->is_using;
+            if ($setting->save()) {
+                $data['success'] = true;
+            } else {
+                $data['success'] = false;
+            }
+        } catch (\Throwable $th) {
+            $data['success'] =  false;
+            $data['errors'] =  collect(['Error']);
+        }
+
+        return response()->json(['data' => $data]);
+    }
 }
--- resources/views/admin/announcement/setting.blade.php
+++ resources/views/admin/announcement/setting.blade.php
@@ -71,7 +71,9 @@
 						<td class="alCenter"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{ $setting->notification_count }}</font></font></td>
 						<td class="alCenter"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">{{ $setting->is_using ? '' : '未' }}使用</font></font></td>
 						<td class="listBtn">
-							<span class="whGraBtn ty2"><button type="button" class="txt" onclick="fnPopWinCT(&#39;./BbSet_W.asp?sKind=M&amp;sBbCode={{ $setting->id }}&#39;, &#39;BbSet_W&#39;, 1100, 800);"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">修改</font></font></button></span>
+							<span class="whGraBtn ty2"><button type="button" class="txt" onclick="fnEdit(`{{ json_encode(array($setting)) }}`)">
+								<font style="vertical-align: inherit;"><font style="vertical-align: inherit;">修改</font></font></button>
+							</span>
 							<!-- <span class="whRedBtn ty2"><button type="button" class="txt" onclick="fnDel_M(&#39;2&#39;);"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">删除</font></font></button></span> -->
 						</td>
 					</tr>
@@ -86,5 +88,111 @@
 <div class="BtmSolution">
 	<h3><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"></font></font><a href="#" target="_blank"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"> </font></font></a></h3>
 </div>
+</div>
+<div id="edit_modal" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+			<div class="modal-header">
+				編輯公告设置
+			</div>
+            <div class="modal-body">
+                <form method="post" name="frmSmsCont" id="frmSmsCont">
+                    <input type="hidden" name="sKind" value="M">
+                    <input type="hidden" name="SMS_SEQ" value="11">
+                    <input type="hidden" name="STATE_SEQ" value="0">
+
+                    <table class="order_write order_table_top">
+                        <colgroup>
+                            <col width="15%">
+                            <col width="35%">
+                            <col width="15%">
+                            <col width="35%">
+                        </colgroup>
+
+                        <tbody>
+							<tr>
+								<th><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">布告栏ID名称</font></font></th>
+								<td>
+									<span id="modal_name"></span>
+								</td>
+								<th><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">类型</font></font></th>
+								<td>
+									<span id="modal_type"></span>
+								</td>
+							</tr>
+							<tr>
+								<th><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">是否使用密码</font></font></th>
+								<td>
+									<select name="USE_YN" id="modal_use_password">
+										<option value="1" selected="">使用</option>
+										<option value="0">未使用</option>
+									</select>
+								</td>
+								<th><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">是否使用标题</font></font></th>
+								<td>
+									<select name="USE_YN" id="modal_use_title">
+										<option value="1" selected="">使用</option>
+										<option value="0">未使用</option>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<th><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">是否使用状态</font></font></th>
+								<td>
+									<select name="USE_YN" id="modal_use_state">
+										<option value="1" selected="">使用</option>
+										<option value="0">未使用</option>
+									</select>
+								</td>
+								<th><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">是否写权限</font></font></th>
+								<td>
+									<select name="USE_YN" id="modal_write_permission">
+										<option value="1" selected="">使用</option>
+										<option value="0">未使用</option>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<th><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">是否查看</font></font></th>
+								<td>
+									<select name="USE_YN" id="modal_view_all">
+										<option value="1" selected="">全部</option>
+										<option value="0">仅限会员</option>
+									</select>
+								</td>
+								<th><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">评论权限</font></font></th>
+								<td>
+									<select name="USE_YN" id="modal_comment_permission">
+										<option value="1" selected="">使用</option>
+										<option value="0">未使用</option>
+									</select>
+								</td>
+							</tr>
+							<tr>
+								<th><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">是否使用</font></font></th>
+								<td span="3">
+									<select name="USE_YN" id="modal_is_using">
+										<option value="1" selected="">使用</option>
+										<option value="0">未使用</option>
+									</select>
+								</td>
+							</tr>
+	                    </tbody>
+					</table>
+                </form>
+            </div>
+
+            <div class="modal-footer">
+                <div class="btn-area alCenter">
+                    <span class="whGraBtn_bg ty2">
+                        <button type="button" class="txt" onclick="fnSave()"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Save</font></font></button>
+                    </span>
+                    <span class="whGraBtn ty2">
+                        <button type="button" class="txt" onclick="fnEditModalClose()" data-dismiss="modal"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Close</font></font></button>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
